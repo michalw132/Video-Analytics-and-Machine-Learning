8e4d6fdd7186b436042267da5d25bf43fd63739c
40367926 <amcgee05@qub.ac.uk>
2025-11-07 12:08:14 +0000

    Added more file structure

---
+++ DatasetSplit/split5050.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = 5050(inputArg1,inputArg2)
+%5050 Summary of this function goes here
+%   Detailed explanation goes here
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
